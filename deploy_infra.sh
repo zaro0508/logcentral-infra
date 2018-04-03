7087168311c3043e79055880e0dca27fd6e3b846
Khai Do <zaro0508@gmail.com>
2018-04-03 13:33:04 -0700

    fix s3 bucket references

--- deploy_infra.sh
+++ deploy_infra.sh
@@ -4,8 +4,8 @@ set -e
 # deploy cloudformation templates
 # Need to upload TEMPLATES to S3 before validating due to template-body MAX 51K length
 # https://docs.aws.amazon.com/cli/latest/reference/cloudformation/validate-template.html#options
-S3_BUCKET="bootstrap-awss3cloudformationbucket-114n2ojlbvj21"
-S3_BUCKET_PATH="scicomp-infra/$TRAVIS_BRANCH"
+S3_BUCKET="bootstrap-awss3cloudformationbucket-mgktko2ect4d"
+S3_BUCKET_PATH="logcentral-infra/$TRAVIS_BRANCH"
 S3_BUCKET_URL="s3://$S3_BUCKET/$S3_BUCKET_PATH"
 TEMP_DIR="temp"
 
@@ -33,12 +33,3 @@ do
 done
 
 popd
-
-# deploy lambda code
-LAMBDA_ARTIFACTS_BUCKET="essentials-awss3lambdaartifactsbucket-1ef8sqdil160e"
-LAMBDA_PACKAGE="jumpcloud.zip"
-pushd lambda/jumpcloud
-zip -r ../$LAMBDA_PACKAGE *
-popd
-echo -e "\nUploading lambda package to s3://$LAMBDA_ARTIFACTS_BUCKET/$S3_BUCKET_PATH/$LAMBDA_PACKAGE"
-aws s3 cp lambda/$LAMBDA_PACKAGE s3://$LAMBDA_ARTIFACTS_BUCKET/$S3_BUCKET_PATH/$LAMBDA_PACKAGE
